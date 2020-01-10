3956b4a9ae071fc439981fa3bf9e8b4cf0acc636
PotterKeyblade <victor.gomez.martinez2@gmail.com>
2020-01-10 20:11:11 +0100

    numero_random.py

---
+++ numero_random.py
@@ -0,0 +1,34 @@
+import random;
+
+random = random.randint(1,20);
+
+nombre=  input("¿Como te llamas?\n")
+print("Hola ", nombre , ", he pensado en jugar a algo. ¿Te parece bien?\n¡Empecemos!")
+print("\n\rEstoy pensando en un número entre 1 y 20.")
+try:
+    num=int(input("¿Cual es?\n"))
+    contador=5
+
+    while num != random and contador>0:
+        if num < random:
+            contador = contador -1
+            print("Te quedan ", contador, " intentos")
+            print("El número que he pensado es más alto");
+            num=int(input("¿Cual es?\n"));
+            
+        if num > random:
+            contador = contador -1
+            print("Te quedan ", contador, " intentos")
+            print("El número que he pensado es más bajo");
+            num=int(input("¿Cual es?\n"));
+
+    if contador==0:
+        print(nombre," eres un perdedor")
+
+    if num == random:
+        print("¡Bien hecho, ", nombre, "!")
+except :
+    print("Algo ha ido mal")
+
+
+
